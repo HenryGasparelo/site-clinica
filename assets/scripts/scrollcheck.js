06ff49ed7052a92189f1bdc1991fe0f4cfc75e18
JoaoPedruFidelis <pedrufidelis@gmail.com>
2023-04-26 22:04:08 -0300

    Co-authored-by: henriquegasparelo@gmail.com <henriquegasparelo@gmail.com>

--- assets/scripts/scrollcheck.js
+++ assets/scripts/scrollcheck.js
@@ -1,11 +1,12 @@
 window.addEventListener("load", (e) => {
     var menu = document.getElementById('menu');
-    menu.classList.add('transparent');
+    menu.classList.add('transparent', 'bef');
 });
 window.addEventListener("scroll", (e) => {
     let scroll = this.scrollY;
     var menu = document.getElementById('menu');
     
+    menu.classList.remove('bef');
     if(scroll > 75){
         menu.classList.remove('transparent');
         menu.classList.add('style');
